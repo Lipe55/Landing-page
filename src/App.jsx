f7dec4b370741e1e3132a7f643699ebd9177644d
Lipe55 <luisfelipecaleal10@gmail.com>
2026-09-23 18:24:21 -0300

    feat: inicia projeto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <p>Atendimento de verdade</p>
+      <h1>Sua Empresa</h1>
+      <p>Um site pronto para mostrar o melhor da sua empresa.</p>
+    </main>
+  );
+}
+
+export default App;
